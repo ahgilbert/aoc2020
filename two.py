afd7d92a7a19d62c244783b5b58d570112cc0eb5
Alan Gilbert <alan@shipwell.com>
2020-12-02 09:29:59 -0600

    look at p2

---
+++ two.py
@@ -0,0 +1,12 @@
+from os import path
+
+if __name__ == '__main__':
+    with open(path.join(path.dirname(__file__), 'two.txt')) as data:
+        passwords = list(data.readlines())
+        # part1_pieces = solve_part_1(numbers, 2020)
+        # print(f"Day 1 part1 answer: {part1_pieces[0]}*{part1_pieces[1]} = {part1_pieces[0]*part1_pieces[1]}")
+
+        # part2_pieces = solve_part_2(numbers, 2020)
+        # print(f"Day 1 part2 answer: {part2_pieces[0]}*{part2_pieces[1]}*{part2_pieces[2]} = {part2_pieces[0]*part2_pieces[1]*part2_pieces[2]}")
+
+        print(len(passwords))
