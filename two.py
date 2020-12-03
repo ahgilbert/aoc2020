3248d797c23705869e28eeae62ba143f0288885c
Alan Gilbert <alan@shipwell.com>
2020-12-03 17:34:55 -0600

    solve d2

--- two.py
+++ two.py
@@ -1,12 +1,32 @@
 from os import path
 
-if __name__ == '__main__':
-    with open(path.join(path.dirname(__file__), 'in2.txt')) as data:
+def parseLine(line):
+    bits = line.split(' ')
+    bounds = bits[0].split('-')
+    target = bits[1][0]
+    password = bits[2]
+
+    return (int(bounds[0]), int(bounds[1]), target, password)
+
+def valid(at_least, at_most, target, password):
+    count = password.count(target)
+    return count >= at_least and count <= at_most
+
+def valid2(idx_a, idx_b, target, password):
+    match_a = password[idx_a - 1] == target
+    match_b = password[idx_b - 1] == target
+    return match_a != match_b
+
+def fetch_input():
+    with open(path.join('.', 'in2.txt')) as data:
         passwords = list(data.readlines())
-        # part1_pieces = solve_part_1(numbers, 2020)
-        # print(f"Day 1 part1 answer: {part1_pieces[0]}*{part1_pieces[1]} = {part1_pieces[0]*part1_pieces[1]}")
+        return passwords
 
-        # part2_pieces = solve_part_2(numbers, 2020)
-        # print(f"Day 1 part2 answer: {part2_pieces[0]}*{part2_pieces[1]}*{part2_pieces[2]} = {part2_pieces[0]*part2_pieces[1]*part2_pieces[2]}")
 
-        print(len(passwords))
+if __name__ == '__main__':
+    passwords = fetch_input()
+    parsed = [parseLine(a) for a in passwords]
+    part1 = [a for a in parsed if valid(*a)]
+    part2 = [a for a in parsed if valid2(*a)]
+    print(len(part1))
+    print(len(part2))
